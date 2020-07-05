39e67e7bd03be3f18985d91c8f6c7f9878955c40
gafurlan <gafurlan@users.noreply.github.com>
2020-07-05 11:19:13 -0300

    definir movimentos

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = []
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
-
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -21,5 +20,28 @@ function criarCobra(){
     }
 }
 
-criarBG();
-criarCobra();
+function iniciarJogo(){
+    criarBG();
+    criarCobra();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
